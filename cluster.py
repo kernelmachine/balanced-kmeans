0090cc2b7a7d7be89f3345e6388966d4eafbd50f
kernelmachine <suching@fb.com>
2021-10-06 16:12:55 -0700

    update

--- cluster.py
+++ cluster.py
@@ -1,12 +1,11 @@
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
-from kmeans_pytorch import kmeans, kmeans_predict, pairwise_distance
+from kmeans_pytorch import KMeans
 from sklearn.decomposition import PCA
 from sklearn import datasets
 from fairseq import libbase
 import pandas as pd
-from sklearn.cluster import KMeans, MiniBatchKMeans
 from scipy.spatial.distance import cdist
 from scipy.optimize import linear_sum_assignment
 import numpy as np
@@ -53,7 +52,12 @@ def get_clusters(X, cluster_size=None, kmeans=None, predict=False, balanced=Fals
 
 def plot_blobs(data,cluster_centers, labels, plot_file):    
     plt.figure(figsize=(4, 3), dpi=160)
+    pca = PCA(n_components=2)
+    master = np.concatenate([data, cluster_centers], 0)
+    pca = pca.fit(master)
+    data = pca.transform(data)
     plt.scatter(data[:, 0], data[:, 1], c=labels)
+    cluster_centers = pca.transform(cluster_centers)
     plt.scatter(
         cluster_centers[:, 0], cluster_centers[:, 1],
         c='white',
@@ -66,22 +70,16 @@ def plot_blobs(data,cluster_centers, labels, plot_file):
     plt.savefig(plot_file, dpi=300)
 
 
-def load_cpp():
-    from fairseq import libbase
-
-    return libbase
-
-
 if __name__ == '__main__':
     seed = 235
-    num_clusters = 100
+    num_clusters = 8
     balanced = True
     # set random seed
     np.random.seed(seed)
 
-    n_samples = 10000
+    n_samples = 8000
     
-    blobs = datasets.make_blobs(n_samples=n_samples, random_state=seed, centers=[[1, 1], [-1, -1], [1, -1]], cluster_std=0.6)
+    blobs = datasets.make_blobs(n_samples=n_samples, n_features=768, random_state=seed, centers=[[1, 1], [-1, -1], [1, -1]], cluster_std=0.6)
 
     X = torch.from_numpy(blobs[0][:n_samples//2])
     y = torch.from_numpy(blobs[0][n_samples//2:])
@@ -90,47 +88,47 @@ if __name__ == '__main__':
         device = torch.device('cuda:0')
     else:
         device = torch.device('cpu')
-    cpp = load_cpp()
 
-    cluster_ids_x, cluster_centers = kmeans(
-        X=X, num_clusters=num_clusters, distance='euclidean', device=device, iter_limit=1000, balanced=balanced
+    kmeans = KMeans(num_clusters=8)
+    _ = kmeans.fit(
+        X=X, num_clusters=num_clusters, distance='euclidean', device=device, iter_limit=25, balanced=balanced
     )
 
 
-    cluster_ids_y = kmeans_predict(
-        X=y, cluster_centers=cluster_centers,  distance='euclidean', device=device
+    
+
+    kmeans.save("model.pkl")
+    kmeans = kmeans.load("model.pkl")
+    cluster_ids_y = kmeans.predict(
+        X=y,  distance='euclidean', device=device
     )
-
-
+    plot_blobs(y, kmeans.cluster_centers, cluster_ids_y, "balanced_clusters.pdf")
     # cluster_ids_x, kmeans = get_clusters(X, cluster_size=n_samples // num_clusters, predict=False, balanced=True)
 
 
+    
 
     
     # cluster_ids_y, kmeans = get_clusters(y,  cluster_size=n_samples // num_clusters, predict=True, kmeans=kmeans, balanced=True)
 
-
+    
     # plot_blobs(X, cluster_centers, cluster_ids_x, "balanced_clusters.pdf")
-    cx = pd.Series(cluster_ids_y)
-    cx = cx.value_counts().sort_index()
+    # cx = pd.Series(cluster_ids_y)
+    # cx = cx.value_counts().sort_index()
 
-    cluster_ids_x, kmeans = get_clusters(X, predict=False, kmeans=KMeans(n_clusters=num_clusters), balanced=False)
-    
+    # cluster_ids_x, kmeans = get_clusters(X, predict=False, kmeans=KMeans(n_clusters=num_clusters), balanced=False)
 
-
-    
-    cluster_ids_y, kmeans = get_clusters(y, predict=True, kmeans=kmeans, balanced=False)
+    # cluster_ids_y, kmeans = get_clusters(y, predict=True, kmeans=kmeans, balanced=False)
 
 
     # plot_blobs(y, kmeans.cluster_centers_, cluster_ids_y, "unbalanced_clusters.pdf")
-    cy = pd.Series(cluster_ids_y)
-    cy = cy.value_counts().sort_index()
-
-    df = pd.DataFrame({"balanced": cx,
-                       "unbalanced": cy})
-    fig, ax = plt.subplots(1,1)
-    ax = sns.boxplot(data=df, ax=ax)
-    ax.set_ylabel("cluster size")
-    ax.set_xlabel("cluster type")
-    plt.savefig("boxplot.pdf", dpi=300)
+    # cy = pd.Series(cluster_ids_y)
+    # cy = cy.value_counts().sort_index()
+    # df = pd.DataFrame({"balanced": cx,
+    #                    "unbalanced": cy})
+    # fig, ax = plt.subplots(1,1)
+    # ax = sns.boxplot(data=df, ax=ax)
+    # ax.set_ylabel("cluster size")
+    # ax.set_xlabel("cluster type")
+    # plt.savefig("boxplot.pdf", dpi=300)
     
--- kmeans_pytorch/__init__.py
+++ kmeans_pytorch/__init__.py
@@ -7,194 +7,222 @@ import pandas as pd
 from .soft_dtw_cuda import SoftDTW
 from scipy.optimize import linear_sum_assignment
 from balanced_assignment import auction_lap
+from sklearn.decomposition import PCA
+import pickle
 
-def initialize(X, num_clusters):
-    """
-    initialize cluster centers
-    :param X: (torch.tensor) matrix
-    :param num_clusters: (int) number of clusters
-    :return: (np.array) initial state
-    """
-    num_samples = len(X)
-    indices = np.random.choice(num_samples, num_clusters, replace=False)
-    initial_state = X[indices]
-    return initial_state
-
-def load_cpp():
-    from fairseq import libbase
-
-    return libbase
 
 def batchify(a, n=2):
     for i in np.array_split(a, n, axis=0):
         yield i
 
-def kmeans(
-        X,
-        num_clusters,
-        distance='euclidean',
-        cluster_centers=[],
-        tol=5e-4,
-        tqdm_flag=True,
-        iter_limit=0,
-        device=torch.device('cpu'),
-        gamma_for_soft_dtw=0.001,
-        balanced=False
-):
-    """
-    perform kmeans
-    :param X: (torch.tensor) matrix
-    :param num_clusters: (int) number of clusters
-    :param distance: (str) distance [options: 'euclidean', 'cosine'] [default: 'euclidean']
-    :param tol: (float) threshold [default: 0.0001]
-    :param device: (torch.device) device [default: cpu]
-    :param tqdm_flag: Allows to turn logs on and off
-    :param iter_limit: hard limit for max number of iterations
-    :param gamma_for_soft_dtw: approaches to (hard) DTW as gamma -> 0
-    :return: (torch.tensor, torch.tensor) cluster ids, cluster centers
-    """
-    if tqdm_flag:
-        print(f'running k-means on {device}..')
 
-    if distance == 'euclidean':
-        pairwise_distance_function = partial(pairwise_distance, device=device, tqdm_flag=tqdm_flag)
-    elif distance == 'cosine':
-        pairwise_distance_function = partial(pairwise_cosine, device=device)
-    elif distance == 'soft_dtw':
-        sdtw = SoftDTW(use_cuda=device.type == 'cuda', gamma=gamma_for_soft_dtw)
-        pairwise_distance_function = partial(pairwise_soft_dtw, sdtw=sdtw, device=device)
-    else:
-        raise NotImplementedError
 
-    # convert to float
-    X = X.float()
-
-    cpp = load_cpp()
+class KMeans(object):
+    def __init__(self, num_clusters, cluster_centers=None):
+        self.num_clusters = num_clusters
+        self.cluster_centers = cluster_centers
     
-    # transfer to device
-    X = X.to(device)
+    @classmethod
+    def load(cls, path_to_file):
+        with open(path_to_file, 'rb') as f:
+            saved = pickle.load(f)
+        return cls(saved['num_clusters'], saved['cluster_centers'])
+    
+    def save(self, path_to_file):
+        with open(path_to_file, 'wb+') as f :
+            pickle.dump(self.__dict__, f)
 
-    # initialize
-    if type(cluster_centers) == list:  # ToDo: make this less annoyingly weird
-        initial_state = initialize(X, num_clusters)
-    else:
+    def initialize(self, X):
+        """
+        initialize cluster centers
+        :param X: (torch.tensor) matrix
+        :param num_clusters: (int) number of clusters
+        :return: (np.array) initial state
+        """
+        num_samples = len(X)
+        indices = np.random.choice(num_samples, self.num_clusters, replace=False)
+        initial_state = X[indices]
+        return initial_state
+    
+    def fit(
+            self,
+            X,
+            num_clusters,
+            distance='euclidean',
+            cluster_centers=[],
+            tol=1e-3,
+            tqdm_flag=True,
+            iter_limit=0,
+            device=torch.device('cpu'),
+            gamma_for_soft_dtw=0.001,
+            balanced=False
+    ):
+        """
+        perform kmeans
+        :param X: (torch.tensor) matrix
+        :param num_clusters: (int) number of clusters
+        :param distance: (str) distance [options: 'euclidean', 'cosine'] [default: 'euclidean']
+        :param tol: (float) threshold [default: 0.0001]
+        :param device: (torch.device) device [default: cpu]
+        :param tqdm_flag: Allows to turn logs on and off
+        :param iter_limit: hard limit for max number of iterations
+        :param gamma_for_soft_dtw: approaches to (hard) DTW as gamma -> 0
+        :return: (torch.tensor, torch.tensor) cluster ids, cluster centers
+        """
         if tqdm_flag:
-            print('resuming')
-        # find data point closest to the initial cluster center
-        initial_state = cluster_centers
-        dis = pairwise_distance_function(X, initial_state)
-        
-        
-        # choice_points = torch.argmin(dis, dim=0)
-        initial_state = X[choice_points]
-        initial_state = initial_state.to(device)
-        
+            print(f'running k-means on {device}..')
 
-    iteration = 0
-    if tqdm_flag:
-        tqdm_meter = tqdm(desc='[running kmeans]')
-    # cluster = torch.arange(num_clusters).repeat_interleave(X.shape[0] // num_clusters).to(device)
-    done=False
-    while True:
-        if balanced:
-            distance_matrix = pairwise_distance_function(X, initial_state)
-            cluster_assignments = auction_lap(-distance_matrix)
-            # ?.repeat_interleave(X.shape[0] // num_clusters)
-            
-            # SCIPY LINEAR ASSIGNMENT SOLVER
-            # cluster_assignments = linear_sum_assignment(-distance_matrix.cpu().numpy(), maximize=True)[1] // (X.shape[0] // num_clusters)   
-            # cluster_assignments = torch.IntTensor(cluster_assignments).cuda()
-
-            ## BASE LAYER ASSIGNMENT
-            
-            # balanced_assignments = auction_lap(-distance_matrix)
-            # cluster_assignments = cluster[balanced_assignments]
-            # from fairseq import pdb; pdb.set_trace()
-
-            
+        if distance == 'euclidean':
+            pairwise_distance_function = partial(pairwise_distance, device=device, tqdm_flag=tqdm_flag)
+        elif distance == 'cosine':
+            pairwise_distance_function = partial(pairwise_cosine, device=device)
+        elif distance == 'soft_dtw':
+            sdtw = SoftDTW(use_cuda=device.type == 'cuda', gamma=gamma_for_soft_dtw)
+            pairwise_distance_function = partial(pairwise_soft_dtw, sdtw=sdtw, device=device)
         else:
-            dis = pairwise_distance_function(X, initial_state)
-            cluster_assignments = torch.argmin(dis, dim=1)
+            raise NotImplementedError
+
+        # convert to float
+        X = X.float()
+
         
-        initial_state_pre = initial_state.clone()
-        for index in range(num_clusters):
-            selected = torch.nonzero(cluster_assignments == index).squeeze().to(device)
+        # transfer to device
+        X = X.to(device)
 
-            selected = torch.index_select(X, 0, selected)
-
-            # https://github.com/subhadarship/kmeans_pytorch/issues/16
-            if selected.shape[0] == 0:
-                selected = X[torch.randint(len(X), (1,))]
+        # initialize
+        if type(cluster_centers) == list:  # ToDo: make this less annoyingly weird
+            initial_state = self.initialize(X)
+        else:
+            if tqdm_flag:
+                print('resuming')
+            # find data point closest to the initial cluster center
+            initial_state = cluster_centers
+            dis = pairwise_distance_function(X, initial_state)
             
-            initial_state[index] = selected.mean(dim=0)
+            
+            # choice_points = torch.argmin(dis, dim=0)
+            initial_state = X[choice_points]
+            initial_state = initial_state.to(device)
+            
 
-        center_shift = torch.sum(
-            torch.sqrt(
-                torch.sum((initial_state - initial_state_pre) ** 2, dim=1)
-            ))
-
-        # increment iteration
-        iteration = iteration + 1
-
-        # update tqdm meter
+        iteration = 0
         if tqdm_flag:
-            tqdm_meter.set_postfix(
-                iteration=f'{iteration}',
-                center_shift=f'{center_shift ** 2:0.6f}',
-                tol=f'{tol:0.6f}'
-            )
-            tqdm_meter.update()
-        if center_shift ** 2 < tol:
-            break
-        if iter_limit != 0 and iteration >= iter_limit:
-            break
+            tqdm_meter = tqdm(desc='[running kmeans]')
+        # cluster = torch.arange(num_clusters).repeat_interleave(X.shape[0] // num_clusters).to(device)
+        done=False
+        while True:
+            if balanced:
+                distance_matrix = pairwise_distance_function(X, initial_state)
+                cluster_assignments = auction_lap(-distance_matrix)
+                
+                # SCIPY LINEAR ASSIGNMENT SOLVER
+                # cluster_assignments = linear_sum_assignment(-distance_matrix.cpu().numpy(), maximize=True)[1] // (X.shape[0] // num_clusters)   
+                # cluster_assignments = torch.IntTensor(cluster_assignments).cuda()
+                
+            else:
+                dis = pairwise_distance_function(X, initial_state)
+                cluster_assignments = torch.argmin(dis, dim=1)
             
+            initial_state_pre = initial_state.clone()
+            for index in range(num_clusters):
+                selected = torch.nonzero(cluster_assignments == index).squeeze().to(device)
 
-    return cluster_assignments.cpu(), initial_state.cpu()
+                selected = torch.index_select(X, 0, selected)
+
+                # https://github.com/subhadarship/kmeans_pytorch/issues/16
+                if selected.shape[0] == 0:
+                    selected = X[torch.randint(len(X), (1,))]
+                
+                initial_state[index] = selected.mean(dim=0)
+
+            center_shift = torch.sum(
+                torch.sqrt(
+                    torch.sum((initial_state - initial_state_pre) ** 2, dim=1)
+                ))
+
+            # increment iteration
+            iteration = iteration + 1
+
+            # update tqdm meter
+            if tqdm_flag:
+                tqdm_meter.set_postfix(
+                    iteration=f'{iteration}',
+                    center_shift=f'{center_shift ** 2:0.6f}',
+                    tol=f'{tol:0.6f}'
+                )
+                tqdm_meter.update()
+            if center_shift ** 2 < tol:
+                break
+            if iter_limit != 0 and iteration >= iter_limit:
+                break
+                
+        self.cluster_centers = initial_state.cpu()
+
+        return cluster_assignments.cpu()
 
 
-def kmeans_predict(
-        X,
-        cluster_centers,
-        distance='euclidean',
-        device=torch.device('cpu'),
-        gamma_for_soft_dtw=0.001,
-        tqdm_flag=True
-):
-    """
-    predict using cluster centers
-    :param X: (torch.tensor) matrix
-    :param cluster_centers: (torch.tensor) cluster centers
-    :param distance: (str) distance [options: 'euclidean', 'cosine'] [default: 'euclidean']
-    :param device: (torch.device) device [default: 'cpu']
-    :param gamma_for_soft_dtw: approaches to (hard) DTW as gamma -> 0
-    :return: (torch.tensor) cluster ids
-    """
-    if tqdm_flag:
-        print(f'predicting on {device}..')
+    def plot(self, data, labels, plot_file):
+        if self.cluster_centers is None:
+            raise Exception("Fit the KMeans object first before plotting!")
+        plt.figure(figsize=(4, 3), dpi=160)
+        pca = PCA(n_components=2)
+        master = np.concatenate([data, self.cluster_centers], 0)
+        pca = pca.fit(master)
+        data = pca.transform(data)
+        plt.scatter(data[:, 0], data[:, 1], c=labels)
+        cluster_centers = pca.transform(cluster_centers)
+        plt.scatter(
+            self.cluster_centers[:, 0], self.cluster_centers[:, 1],
+            c='white',
+            alpha=0.6,
+            edgecolors='black',
+            linewidths=2
+        )
+        plt.tight_layout()
+        plt.savefig(plot_file, dpi=300)
 
-    if distance == 'euclidean':
-        pairwise_distance_function = partial(pairwise_distance, device=device, tqdm_flag=tqdm_flag)
-    elif distance == 'cosine':
-        pairwise_distance_function = partial(pairwise_cosine, device=device)
-    elif distance == 'soft_dtw':
-        sdtw = SoftDTW(use_cuda=device.type == 'cuda', gamma=gamma_for_soft_dtw)
-        pairwise_distance_function = partial(pairwise_soft_dtw, sdtw=sdtw, device=device)
-    else:
-        raise NotImplementedError
 
-    # convert to float
-    X = X.float()
+    def predict(
+            self,
+            X,
+            distance='euclidean',
+            device=torch.device('cpu'),
+            gamma_for_soft_dtw=0.001,
+            tqdm_flag=True
+    ):
+        """
+        predict using cluster centers
+        :param X: (torch.tensor) matrix
+        :param cluster_centers: (torch.tensor) cluster centers
+        :param distance: (str) distance [options: 'euclidean', 'cosine'] [default: 'euclidean']
+        :param device: (torch.device) device [default: 'cpu']
+        :param gamma_for_soft_dtw: approaches to (hard) DTW as gamma -> 0
+        :return: (torch.tensor) cluster ids
+        """
+        if tqdm_flag:
+            print(f'predicting on {device}..')
 
-    # transfer to device
-    X = X.to(device)
-    cpp = load_cpp()
-    distance_matrix = pairwise_distance_function(X, cluster_centers)
-    cluster_assignments = auction_lap(-distance_matrix)
-    # dis = pairwise_distance_function(X, cluster_centers)
-    # cluster_assignments = torch.argmin(dis, dim=1)
+        if distance == 'euclidean':
+            pairwise_distance_function = partial(pairwise_distance, device=device, tqdm_flag=tqdm_flag)
+        elif distance == 'cosine':
+            pairwise_distance_function = partial(pairwise_cosine, device=device)
+        elif distance == 'soft_dtw':
+            sdtw = SoftDTW(use_cuda=device.type == 'cuda', gamma=gamma_for_soft_dtw)
+            pairwise_distance_function = partial(pairwise_soft_dtw, sdtw=sdtw, device=device)
+        else:
+            raise NotImplementedError
 
-    return cluster_assignments.cpu()
+        # convert to float
+        X = X.float()
+
+        # transfer to device
+        X = X.to(device)
+        distance_matrix = pairwise_distance_function(X, self.cluster_centers)
+        cluster_assignments = auction_lap(-distance_matrix)
+        # dis = pairwise_distance_function(X, cluster_centers)
+        # cluster_assignments = torch.argmin(dis, dim=1)
+
+        return cluster_assignments.cpu()
 
 
 def pairwise_distance(data1, data2, device=torch.device('cpu'), tqdm_flag=True):
